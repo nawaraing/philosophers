1e241936bf3d973e95e7414224642c10e77501bb
nawaraing <nawaddaing@gmail.com>
2021-02-17 16:07:04 +0900

    Work well philo_one
    but norm yet:

--- philo_one/main.c
+++ philo_one/main.c
@@ -1,6 +1,6 @@
 #include "philo_one.h"
 
-t_data	g_data;
+t_data			g_data;
 
 unsigned long	cur_time(void)
 {
@@ -11,7 +11,7 @@ unsigned long	cur_time(void)
 	return (time.tv_sec * 1000000 + time.tv_usec);
 }
 
-int		init_data(int argc, char *argv[])
+int				init_data(int argc, char *argv[])
 {
 	int				i;
 
@@ -25,13 +25,14 @@ int		init_data(int argc, char *argv[])
 		g_data.number_of_times_each_philo_must_eat = ft_atoi(argv[5]);
 	else
 		g_data.number_of_times_each_philo_must_eat = -1;
-	g_data.is_someone_dead = 0;
+	g_data.is_end = 0;
 	g_data.start_time = cur_time();
 	g_data.last_time = g_data.start_time;
 	i = 0;
 	while (++i <= g_data.number_of_philo)
 	{
 		g_data.last_eat[i] = g_data.start_time;
+		g_data.eat_cnt[i] = 0;
 		pthread_mutex_init(&(g_data.mutex[i]), NULL);
 	}
 	return (0);
@@ -51,56 +52,52 @@ void			new_sleep(int usec)
 	}
 }
 
-int		check_someone_die(void)
+int				check_someone_die(void)
 {
-	return (g_data.is_someone_dead);
+	return (g_data.is_end);
 }
-
-int		check_i_die(int philo_id)
-{
-	if (g_data.last_time - g_data.last_eat[philo_id] > g_data.time_to_die)
-	{
-		g_data.is_someone_dead = 1;
-		printf("%lu %d died\n", (cur_time() - g_data.start_time) / 1000, philo_id);
-		return (1);
-	}
-	return (0);
-}
-
 
 int		do_eat(int philo_id)
 {
 	if (check_someone_die())
 		return (-1);
-	if (philo_id == 0)
-		pthread_mutex_lock(&(g_data.mutex[g_data.number_of_philo - 1]));
+	if (philo_id == 1)
+		pthread_mutex_lock(&(g_data.mutex[g_data.number_of_philo]));
 	else
 		pthread_mutex_lock(&(g_data.mutex[philo_id - 1]));
-	printf("%lu %d has taken a fork\n", (cur_time() - g_data.start_time) / 1000, philo_id);
+	if (check_someone_die() == 0)
+		printf("%lu %d has taken a fork\n", (cur_time() - g_data.start_time) / 1000, philo_id);
 	pthread_mutex_lock(&(g_data.mutex[philo_id]));
-	printf("%lu %d has taken a fork\n", (cur_time() - g_data.start_time) / 1000, philo_id);
-	printf("%lu %d is eating\n", (cur_time() - g_data.start_time) / 1000, philo_id);
+	if (check_someone_die() == 0)
+	{
+		printf("%lu %d is eating\n", (cur_time() - g_data.start_time) / 1000, philo_id);
+		g_data.eat_cnt[philo_id]++;
+	}
 	g_data.last_eat[philo_id] = cur_time();
 	new_sleep(g_data.time_to_eat);
-	if (philo_id == 0)
-		pthread_mutex_unlock(&(g_data.mutex[g_data.number_of_philo - 1]));
+	if (philo_id == 1)
+		pthread_mutex_unlock(&(g_data.mutex[g_data.number_of_philo]));
 	else
 		pthread_mutex_unlock(&(g_data.mutex[philo_id - 1]));
 	pthread_mutex_unlock(&(g_data.mutex[philo_id]));
+	if (check_someone_die())
+		return (-1);
 	return (0);
 }
 
-int		do_sleep_and_think(int philo_id)
+int				do_sleep_and_think(int philo_id)
 {
 	if (check_someone_die())
 		return (-1);
 	printf("%lu %d is sleeping\n", (cur_time() - g_data.start_time) / 1000, philo_id);
 	new_sleep(g_data.time_to_sleep);
+	if (check_someone_die())
+		return (-1);
 	printf("%lu %d is thinking\n", (cur_time() - g_data.start_time) / 1000, philo_id);
 	return (0);
 }
 
-void	*each_philo(void *philo_id)
+void			*each_philo(void *philo_id)
 {
 	while (1)
 	{
@@ -112,7 +109,7 @@ void	*each_philo(void *philo_id)
 	return (NULL);
 }
 
-void	create_philo(void)
+void			create_philo(void)
 {
 	int					i;
 	pthread_t			thread[201];
@@ -120,53 +117,76 @@ void	create_philo(void)
 	int					result;
 
 	i = 0;
-	printf("number of philo: %d\n", g_data.number_of_philo);
-	while (++i <= g_data.number_of_philo)
+	while (++i * 2 - 1 <= g_data.number_of_philo)
 	{
-		philo[i] = i;
-		pthread_create(&thread[i], NULL, each_philo, (void *)&(philo[i]));
-		printf("philo: %d\n", i);
+		philo[i * 2 - 1] = i * 2 - 1;
+		pthread_create(&thread[i * 2 - 1], NULL, each_philo, (void *)&(philo[i * 2 - 1]));
+	}
+	new_sleep(1);
+	i = 0;
+	while (++i * 2 <= g_data.number_of_philo)
+	{
+		philo[i * 2] = i * 2;
+		pthread_create(&thread[i * 2], NULL, each_philo, (void *)&(philo[i * 2]));
 	}
 	i = 0;
 	while (++i <= g_data.number_of_philo)
 	{
-		pthread_join(thread[i], (void *)&result);
+		pthread_join(thread[i], NULL);
 	}
 }
 
-void		*work_supervisor(void *trash)
+void			*work_supervisor(void *trash)
 {
 	int		i;
+	int		still_hungry;
 
 	while (1)
 	{
 		i = 0;
+		still_hungry = 0;
+		if (g_data.number_of_times_each_philo_must_eat == -1)
+			still_hungry = 1;
 		while (++i <= g_data.number_of_philo)
 		{
 			if (g_data.last_eat[i] + 1000 * g_data.time_to_die < cur_time())
 			{
-				g_data.is_someone_dead = 1;
+				printf("%lu %d died\n", (cur_time() - g_data.start_time) / 1000, i);
+				g_data.is_end = 1;
 				return (NULL);
 			}
+			if (g_data.eat_cnt[i] < g_data.number_of_times_each_philo_must_eat)
+				still_hungry = 1;
+		}
+		if (still_hungry == 0)
+		{
+			g_data.is_end = 1;
+			return (NULL);
 		}
 	}
 	return (NULL);
 }
 
-void		create_supervisor(void)
+void			destroy_data(void)
+{
+	int		i;
+
+	i = 0;
+	while (++i <= g_data.number_of_philo)
+		pthread_mutex_destroy(&(g_data.mutex[i]));
+}
+
+
+int				main(int argc, char *argv[])
 {
 	pthread_t			thread;
 	int					result;
 
-	pthread_create(&thread, NULL, work_supervisor, NULL);
-	pthread_join(thread, (void *)&result);
-}
-
-int			main(int argc, char *argv[])
-{
 	if (init_data(argc, argv) < 0)
 		return (0);
-	create_supervisor();
+	pthread_create(&thread, NULL, work_supervisor, NULL);
 	create_philo();
+	pthread_join(thread, NULL);
+	destroy_data();
 	return (0);
 }
--- philo_one/philo_one.h
+++ philo_one/philo_one.h
@@ -9,6 +9,10 @@
 # include	<string.h>
 # include	<limits.h>
 
+# define 	BLUE	"\033[34m"
+# define	UNSET	"\033[0m"
+
+
 typedef struct	s_data
 {
 	int					number_of_philo;
@@ -17,10 +21,11 @@ typedef struct	s_data
 	int					time_to_sleep;
 	int					number_of_times_each_philo_must_eat;	// 없으면 -1
 
-	int					is_someone_dead;
+	int					is_end;
 	unsigned long		start_time;
 	unsigned long		last_time;
 	unsigned long		last_eat[200];
+	int					eat_cnt[200];
 	pthread_mutex_t		mutex[200];
 }				t_data;
 
